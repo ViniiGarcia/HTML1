35a864d082608f16daf92af00a37ae03bc94788a
ViniiGarcia <viniciusgsgarcia@hotmail.com>
2023-12-07 12:06:48 -0300

    add

---
+++ LEN.py
@@ -0,0 +1,8 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+quantidadeDeMusicos = len(lista_musicos)
+
+print("A lista tem um total de: " + str(quantidadeDeMusicos) + " Musicos")
+print("Dentre eles estão 3 abaixo:")
+print(lista_musicos[2])
+print(lista_musicos[9])
+print(lista_musicos[14])
