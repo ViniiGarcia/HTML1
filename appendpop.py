37df9f766ea12eec5d2fc083101c0b75b6faebbc
ViniiGarcia <viniciusgsgarcia@hotmail.com>
2023-12-07 16:34:13 -0300

    append e pop

---
+++ appendpop.py
@@ -0,0 +1,9 @@
+#Loja
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+print(lista_produtos)
+lista_produtos.append("paleta de sombras")
+lista_produtos.append("espelhos")
+print(lista_produtos)
+lista_produtos.pop()
+print(lista_produtos)
