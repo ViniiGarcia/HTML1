c649e7d0b91d53f44380c97ce74234dc4d50b905
ViniiGarcia <viniciusgsgarcia@hotmail.com>
2023-12-07 14:19:54 -0300

    loja

---
+++ LojaProdutos.py
@@ -0,0 +1,5 @@
+#Loja
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+for i in range (len(lista_produtos)):
+  print("Temos " + lista_produtos[i] + " à venda!")
